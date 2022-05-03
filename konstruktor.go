86355d3fc1b37679dcf084613a8d0f509b1bab10
MichiH <mh@pixx.io>
2022-05-03 16:57:06 +0200

    asdasd

--- konstruktor.go
+++ konstruktor.go
@@ -8,13 +8,13 @@ import (
 )
 
 type RandomData struct {
-	a string
-	b string
-	c string
-	d string
-	e string
-	f string
-	g string
+	A string `protobuf:"bytes,1,opt,name=a,proto3" json:"a,omitempty"`
+	B string `protobuf:"bytes,2,opt,name=b,proto3" json:"b,omitempty"`
+	C string `protobuf:"bytes,3,opt,name=c,proto3" json:"c,omitempty"`
+	D string `protobuf:"bytes,4,opt,name=d,proto3" json:"d,omitempty"`
+	E string `protobuf:"bytes,5,opt,name=e,proto3" json:"e,omitempty"`
+	F string `protobuf:"bytes,6,opt,name=f,proto3" json:"f,omitempty"`
+	G string `protobuf:"bytes,7,opt,name=g,proto3" json:"g,omitempty"`
 }
 
 func init() {
@@ -36,7 +36,7 @@ func CreateBigData(lengthString int, lengthSlice int) []RandomData {
 	bigdata := []RandomData{}
 
 	for i := 0; i < lengthSlice; i++ {
-		random := RandomData{a: RandStringRunes(lengthString), b: RandStringRunes(lengthString), c: RandStringRunes(lengthString), d: RandStringRunes(lengthString), e: RandStringRunes(lengthString), f: RandStringRunes(lengthString), g: RandStringRunes(lengthString)}
+		random := RandomData{A: RandStringRunes(lengthString), B: RandStringRunes(lengthString), C: RandStringRunes(lengthString), D: RandStringRunes(lengthString), E: RandStringRunes(lengthString), F: RandStringRunes(lengthString), G: RandStringRunes(lengthString)}
 		bigdata = append(bigdata, random)
 	}
 
