50005b772a665e4703ec6cbab4615c118a978202
MichiH <mh@pixx.io>
2022-05-23 12:21:35 +0200

    new data format

--- konstruktor.go
+++ konstruktor.go
@@ -8,13 +8,56 @@ import (
 )
 
 type RandomData struct {
-	A string `protobuf:"bytes,1,opt,name=a,proto3" json:"a,omitempty"`
-	B string `protobuf:"bytes,2,opt,name=b,proto3" json:"b,omitempty"`
-	C string `protobuf:"bytes,3,opt,name=c,proto3" json:"c,omitempty"`
-	D string `protobuf:"bytes,4,opt,name=d,proto3" json:"d,omitempty"`
-	E string `protobuf:"bytes,5,opt,name=e,proto3" json:"e,omitempty"`
-	F string `protobuf:"bytes,6,opt,name=f,proto3" json:"f,omitempty"`
-	G string `protobuf:"bytes,7,opt,name=g,proto3" json:"g,omitempty"`
+	Aaabcdef int32  `json:"aaabcdef,omitempty"`
+	Ababcdef bool   `json:"ababcdef,omitempty"`
+	Acabcdef bool   `json:"acabcdef,omitempty"`
+	Adabcdef bool   `json:"adabcdef,omitempty"`
+	Aeabcdef bool   `json:"aeabcdef,omitempty"`
+	Afabcdef bool   `json:"afabcdef,omitempty"`
+	Agabcdef bool   `json:"agabcdef,omitempty"`
+	Ahabcdef bool   `json:"ahabcdef,omitempty"`
+	Aiabcdef bool   `json:"aiabcdef,omitempty"`
+	Ajabcdef bool   `json:"ajabcdef,omitempty"`
+	Akabcdef bool   `json:"akabcdef,omitempty"`
+	Alabcdef string `json:"alabcdef,omitempty"`
+	Amabcdef string `json:"amabcdef,omitempty"`
+	Anabcdef string `json:"anabcdef,omitempty"`
+	Aoabcdef string `json:"aoabcdef,omitempty"`
+	Apabcdef string `json:"apabcdef,omitempty"`
+	Aqabcdef string `json:"aqabcdef,omitempty"`
+	Arabcdef string `json:"arabcdef,omitempty"`
+	Asabcdef string `json:"asabcdef,omitempty"`
+	Atabcdef string `json:"atabcdef,omitempty"`
+	Auabcdef string `json:"auabcdef,omitempty"`
+	Avabcdef string `json:"avabcdef,omitempty"`
+	Awabcdef string `json:"awabcdef,omitempty"`
+	Axabcdef string `json:"axabcdef,omitempty"`
+	Ayabcdef string `json:"ayabcdef,omitempty"`
+	Azabcdef string `json:"azabcdef,omitempty"`
+	Baabcdef string `json:"baabcdef,omitempty"`
+	Bbabcdef string `json:"bbabcdef,omitempty"`
+	Bcabcdef string `json:"bcabcdef,omitempty"`
+	Bdabcdef string `json:"bdabcdef,omitempty"`
+	Beabcdef string `json:"beabcdef,omitempty"`
+	Bfabcdef string `json:"bfabcdef,omitempty"`
+	Bgabcdef string `json:"bgabcdef,omitempty"`
+	Bhabcdef string `json:"bhabcdef,omitempty"`
+	Biabcdef string `json:"biabcdef,omitempty"`
+	Bjabcdef string `json:"bjabcdef,omitempty"`
+	Bkabcdef string `json:"bkabcdef,omitempty"`
+	Blabcdef string `json:"blabcdef,omitempty"`
+	Bmabcdef string `json:"bmabcdef,omitempty"`
+	Bnabcdef string `json:"bnabcdef,omitempty"`
+	Boabcdef string `json:"boabcdef,omitempty"`
+	Bpabcdef string `json:"bpabcdef,omitempty"`
+	Bqabcdef string `json:"bqabcdef,omitempty"`
+	Brabcdef string `json:"brabcdef,omitempty"`
+	Bsabcdef string `json:"bsabcdef,omitempty"`
+	Btabcdef string `json:"btabcdef,omitempty"`
+	Buabcdef string `json:"buabcdef,omitempty"`
+	Bvabcdef string `json:"bvabcdef,omitempty"`
+	Bwabcdef string `json:"bwabcdef,omitempty"`
+	Bxabcdef string `json:"bxabcdef,omitempty"`
 }
 
 func init() {
@@ -36,8 +79,58 @@ func CreateBigData(lengthString int, lengthSlice int) []RandomData {
 	bigdata := []RandomData{}
 
 	for i := 0; i < lengthSlice; i++ {
-		random := RandomData{A: RandStringRunes(lengthString), B: RandStringRunes(lengthString), C: RandStringRunes(lengthString), D: RandStringRunes(lengthString), E: RandStringRunes(lengthString), F: RandStringRunes(lengthString), G: RandStringRunes(lengthString)}
+		random := RandomData{Aaabcdef: int32(rand.Intn(8999999) + 1000000),
+			Ababcdef: true,
+			Acabcdef: true,
+			Adabcdef: false,
+			Aeabcdef: false,
+			Afabcdef: false,
+			Agabcdef: false,
+			Ahabcdef: false,
+			Aiabcdef: false,
+			Ajabcdef: false,
+			Akabcdef: false,
+			Alabcdef: "",
+			Amabcdef: "",
+			Anabcdef: "",
+			Aoabcdef: "",
+			Apabcdef: "",
+			Aqabcdef: "",
+			Arabcdef: "",
+			Asabcdef: "",
+			Atabcdef: "",
+			Auabcdef: "",
+			Avabcdef: "",
+			Awabcdef: RandStringRunes(lengthString),
+			Axabcdef: RandStringRunes(lengthString),
+			Ayabcdef: RandStringRunes(lengthString),
+			Azabcdef: RandStringRunes(lengthString),
+			Baabcdef: RandStringRunes(lengthString),
+			Bbabcdef: RandStringRunes(lengthString),
+			Bcabcdef: RandStringRunes(lengthString),
+			Bdabcdef: RandStringRunes(lengthString),
+			Beabcdef: RandStringRunes(lengthString),
+			Bfabcdef: RandStringRunes(lengthString),
+			Bgabcdef: RandStringRunes(lengthString),
+			Bhabcdef: RandStringRunes(lengthString),
+			Biabcdef: RandStringRunes(lengthString),
+			Bjabcdef: RandStringRunes(lengthString),
+			Bkabcdef: RandStringRunes(lengthString),
+			Blabcdef: RandStringRunes(lengthString),
+			Bmabcdef: RandStringRunes(lengthString),
+			Bnabcdef: RandStringRunes(lengthString),
+			Boabcdef: RandStringRunes(lengthString),
+			Bpabcdef: RandStringRunes(lengthString),
+			Bqabcdef: RandStringRunes(lengthString),
+			Brabcdef: RandStringRunes(lengthString),
+			Bsabcdef: RandStringRunes(lengthString),
+			Btabcdef: RandStringRunes(lengthString),
+			Buabcdef: RandStringRunes(lengthString),
+			Bvabcdef: RandStringRunes(lengthString),
+			Bwabcdef: RandStringRunes(lengthString),
+			Bxabcdef: RandStringRunes(lengthString)}
 		bigdata = append(bigdata, random)
+
 	}
 
 	return bigdata
@@ -49,7 +142,45 @@ func CreateBigData_proto(lengthString int, lengthSlice int) []*pb.RandomData {
 	bigdata := []*pb.RandomData{}
 
 	for i := 0; i < lengthSlice; i++ {
-		random := &pb.RandomData{A: RandStringRunes(lengthString), B: RandStringRunes(lengthString), C: RandStringRunes(lengthString), D: RandStringRunes(lengthString), E: RandStringRunes(lengthString), F: RandStringRunes(lengthString), G: RandStringRunes(lengthString)}
+		random := &pb.RandomData{Aaabcdef: int32(rand.Intn(8999999) + 1000000),
+			Ababcdef: true,
+			Acabcdef: true,
+			Adabcdef: false,
+			Aeabcdef: false,
+			Afabcdef: false,
+			Agabcdef: false,
+			Ahabcdef: false,
+			Aiabcdef: false,
+			Ajabcdef: false,
+			Akabcdef: false,
+			Awabcdef: RandStringRunes(lengthString),
+			Axabcdef: RandStringRunes(lengthString),
+			Ayabcdef: RandStringRunes(lengthString),
+			Azabcdef: RandStringRunes(lengthString),
+			Baabcdef: RandStringRunes(lengthString),
+			Bbabcdef: RandStringRunes(lengthString),
+			Bcabcdef: RandStringRunes(lengthString),
+			Bdabcdef: RandStringRunes(lengthString),
+			Beabcdef: RandStringRunes(lengthString),
+			Bfabcdef: RandStringRunes(lengthString),
+			Bgabcdef: RandStringRunes(lengthString),
+			Bhabcdef: RandStringRunes(lengthString),
+			Biabcdef: RandStringRunes(lengthString),
+			Bjabcdef: RandStringRunes(lengthString),
+			Bkabcdef: RandStringRunes(lengthString),
+			Blabcdef: RandStringRunes(lengthString),
+			Bmabcdef: RandStringRunes(lengthString),
+			Bnabcdef: RandStringRunes(lengthString),
+			Boabcdef: RandStringRunes(lengthString),
+			Bpabcdef: RandStringRunes(lengthString),
+			Bqabcdef: RandStringRunes(lengthString),
+			Brabcdef: RandStringRunes(lengthString),
+			Bsabcdef: RandStringRunes(lengthString),
+			Btabcdef: RandStringRunes(lengthString),
+			Buabcdef: RandStringRunes(lengthString),
+			Bvabcdef: RandStringRunes(lengthString),
+			Bwabcdef: RandStringRunes(lengthString),
+			Bxabcdef: RandStringRunes(lengthString)}
 		bigdata = append(bigdata, random)
 	}
 
